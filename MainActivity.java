e36ba769bbcfa0c95091cc14f20c7bc6401eb45d
Haider1879 <nasirijaz237@gmail.com>
2025-12-29 19:37:55 +0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -5,5 +5,15 @@ btnDeleteAccount.setOnClickListener(v -> {
         Toast.makeText(this, "Account Deleted", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(this, LoginActivity.class));
         finish();
+ 
+    
+    db.collection("books")
+  .document(book.id)
+  .update("title", newTitle, "author", newAuthor);
+db.collection("books")
+  .document(book.id)
+  .delete();
     });
 });
+
+
